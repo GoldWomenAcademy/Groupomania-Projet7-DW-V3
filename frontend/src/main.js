9a008024cecd277ef51a2a4eb19fd48f4885e944
GoldwomenAcademy <fati.pereira74@gmail.com>
2020-10-21 14:17:47 +0200

    update app.vue main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import MicroModal from 'micromodal'; 
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'http://localhost:3000/api/';
+const token = sessionStorage.getItem('token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+}
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+MicroModal.init();
